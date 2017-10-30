a7cc6a69ec432795e7e9c22cd97faf3acfce6c66
Jaacii <sliphno@hotmail.de>
2017-10-30 19:40:22 +0100

    Wissbegierig Schritt 9

--- Studienberatung.java
+++ Studienberatung.java
@@ -7,8 +7,11 @@ public static void main (String[] args) {
 Scanner input = new Scanner (System.in) ;
 	String abi;
 	
+	String wiss;
 	
 	boolean loop = true;
+	
+	boolean loop3 = true;
 	
 	
 //Frage 1 
@@ -34,5 +37,62 @@ Scanner input = new Scanner (System.in) ;
 
 
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Frage 3
+	System.out.println("Bist du Wissbegierig Y N");											
+	while (loop3 == true) {
+	wiss = input.next();	
+	if (wiss.equals("y") || wiss.equals("Y") ) {
+	wiss = "Ja";  
+	System.out.println (wiss); 
+	loop3 = false;
+	
+	}
+	 else if (wiss.equals("N") || wiss.equals("n")) {
+		wiss = "Nein";  
+		System.out.println (wiss); 
+		loop3 = false;
+		
+		}
+	else { System.out.println("Falsche Eingabe. Bitte Y oder N antworten.");
+	loop3 = true;
+	}
+	
+	
+	
+	
+	
 }
 }
