20034608606634148ec8041ad1421b8838c115e7
Jaacii <sliphno@hotmail.de>
2017-10-30 19:37:18 +0100

    master reset, habe Fachabitur ins master statt in den einzelnen Branch commited

--- Studienberatung.java
+++ Studienberatung.java
@@ -19,38 +19,19 @@ Scanner input = new Scanner (System.in) ;
 	abi = "Ja";  
 	System.out.println (abi); 
 	loop = false;
-	loop2 = true;
+	
 	}
 	 else if (abi.equals("N") || abi.equals("n")) {
 		abi = "Nein";  
 		System.out.println (abi); 
 		loop = false;
-		loop2 = true;
+		
 		}
 	else { System.out.println("Falsche Eingabe. Bitte Y oder N antworten.");
 	loop = true;
 	}
 
 
-//Frage 2
-	System.out.println("Hast du Fachabitur Y N ");											
-	while (loop == true) {
-	fachabi = input.next();	
-	if (fachabi.equals("y") || fachabi.equals("Y") ) {
-	fachabi = "Ja";  
-	System.out.println (fachabi); 
-	loop2 = false;
-	
-	}
-	 else if (fachabi.equals("N") || fachabi.equals("n")) {
-		fachabi = "Nein";  
-		System.out.println (fachabi); 
-		loop2 = false;
-		
-		}
-	else { System.out.println("Falsche Eingabe. Bitte Y oder N antworten.");
-	loop2 = true;
-	}
 
 
 }
