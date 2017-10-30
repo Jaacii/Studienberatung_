fe7fd795df339db5ed26ccd449c50c586f3d6e8f
Jaacii <sliphno@hotmail.de>
2017-10-30 19:34:43 +0100

    Fachabitur Schritt 8

--- Studienberatung.java
+++ Studienberatung.java
@@ -6,10 +6,10 @@ public static void main (String[] args) {
 
 Scanner input = new Scanner (System.in) ;
 	String abi;
-	
+	String fachabi;
 	
 	boolean loop = true;
-	
+	boolean loop2 = false;
 	
 //Frage 1 
 	System.out.println("Hast du Abitur Y N ");											
@@ -19,19 +19,38 @@ Scanner input = new Scanner (System.in) ;
 	abi = "Ja";  
 	System.out.println (abi); 
 	loop = false;
-	
+	loop2 = true;
 	}
 	 else if (abi.equals("N") || abi.equals("n")) {
 		abi = "Nein";  
 		System.out.println (abi); 
 		loop = false;
-		
+		loop2 = true;
 		}
 	else { System.out.println("Falsche Eingabe. Bitte Y oder N antworten.");
 	loop = true;
 	}
 
 
+//Frage 2
+	System.out.println("Hast du Fachabitur Y N ");											
+	while (loop == true) {
+	fachabi = input.next();	
+	if (fachabi.equals("y") || fachabi.equals("Y") ) {
+	fachabi = "Ja";  
+	System.out.println (fachabi); 
+	loop2 = false;
+	
+	}
+	 else if (fachabi.equals("N") || fachabi.equals("n")) {
+		fachabi = "Nein";  
+		System.out.println (fachabi); 
+		loop2 = false;
+		
+		}
+	else { System.out.println("Falsche Eingabe. Bitte Y oder N antworten.");
+	loop2 = true;
+	}
 
 
 }
