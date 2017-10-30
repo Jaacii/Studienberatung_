261bac2f8bce20f13a585b839365864934aaf58c
Jaacii <sliphno@hotmail.de>
2017-10-30 19:28:01 +0100

    Phase 4

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,8 +5,31 @@ public class Studienberatung {
 public static void main (String[] args) { 
 
 Scanner input = new Scanner (System.in) ;
-
-
+	String abi;
+	
+	
+	boolean loop = true;
+	
+	
+//Frage 1 
+	System.out.println("Hast du Abitur Y N ");											
+	while (loop == true) {
+	abi = input.next();	
+	if (abi.equals("y") || abi.equals("Y") ) {
+	abi = "Ja";  
+	System.out.println (abi); 
+	loop = false;
+	
+	}
+	 else if (abi.equals("N") || abi.equals("n")) {
+		abi = "Nein";  
+		System.out.println (abi); 
+		loop = false;
+		
+		}
+	else { System.out.println("Falsche Eingabe. Bitte Y oder N antworten.");
+	loop = true;
+	}
 
 
 
